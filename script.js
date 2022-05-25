fafc2f2450441009108fcce471723f0074bfdb77
nagabhishek <nag.abhishek2012@gmail.com>
2022-05-25 17:08:09 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-console.log('hello!');
-
-let count = 1;
 function getData() {
-  console.log('Getting data...', count++);
+  console.log('Getting data...');
+}
+
+function debounce(func, wait) {
+  let timeout;
+  return (...arg) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, arg), wait);
+  };
+}
+
+function throttle(callback, interval) {
+  let enableCall = false;
+  return (...args) => {
+    if (!enableCall) {
+      enableCall = true;
+      callback.apply(this, args);
+      setTimeout(() => (enableCall = false), interval);
+    }
+  };
+}
+
+function better() {
+  debounce(getData, 500);
 }
